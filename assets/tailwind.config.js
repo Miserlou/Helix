328c8426a740c676b0581c767501d65b6ebddc92
Rich Jones <miserlou@gmail.com>
2022-12-20 00:48:36 +0100

    sketch connie, kill banner

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -19,5 +19,8 @@ module.exports = {
     plugin(({addVariant}) => addVariant('phx-click-loading', ['&.phx-click-loading', '.phx-click-loading &'])),
     plugin(({addVariant}) => addVariant('phx-submit-loading', ['&.phx-submit-loading', '.phx-submit-loading &'])),
     plugin(({addVariant}) => addVariant('phx-change-loading', ['&.phx-change-loading', '.phx-change-loading &']))
-  ]
+  ],
+  daisyui: {
+    logs: false
+  }
 }
